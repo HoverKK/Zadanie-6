ba9436aa0a19799b9ac56f5f56d6c0da298be0b5
HoverKK <147485931+HoverKK@users.noreply.github.com>
2024-01-24 12:56:12 +0100

    DataTree.h

---
+++ DataTree.h
@@ -0,0 +1,24 @@
+#ifndef DATATREE_H
+#define DATATREE_H
+
+#include <map>
+#include <vector>
+#include <functional>
+#include "DataPoint.h"
+
+class DataTree {
+public:
+    std::map<int, std::map<int, std::map<int, std::map<int, std::vector<DataPoint>>>>> tree;
+
+    void addDataPoint(const DataPoint& dataPoint);
+    void parseDateTime(const std::string& dateTime, int& year, int& month, int& day, int& quarter);
+    int getQuarter(int hour, int minute);
+    double sumAutokonsumpcja(const std::string& startDateTime, const std::string& endDateTime);
+    double sumEksport(const std::string& startDateTime, const std::string& endDateTime);
+    double sumImport(const std::string& startDateTime, const std::string& endDateTime);
+    double sumPobor(const std::string& startDateTime, const std::string& endDateTime);
+    double sumProdukcja(const std::string& startDateTime, const std::string& endDateTime);
+    void processTimeRange(const std::string& startDateTime, const std::string& endDateTime, const std::function<void(const DataPoint&)>& func);
+};
+
+#endif
